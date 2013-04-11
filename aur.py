a321d3f369d36a9507356f2f78f6c79d7d6f0958
Chris Down <chris@chrisdown.name>
2013-04-11 14:13:22 +0800

    Add code to connect to AUR.

--- aur.py
+++ aur.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 
+import http.client
+
 class QueryTooShortError(Exception):
     """Raised when the query string is too short."""
     pass
+
+class AURClient(object):
+    """Handles client requests to AUR."""
+    def __init__(self):
+        self.host = "aur.archlinux.org"
+        self.apiPath = "/rpc.php"
+        self.c = self._connect()
+
+    def _connect(self):
+        """Initialise connection to AUR."""
+        return http.client.HTTPSConnection(self.host)
