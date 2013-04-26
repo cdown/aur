d448c0f5af7d874342f61c191d6aa80fe1d9fa45
Chris Down <chris@chrisdown.name>
2013-04-26 22:17:45 +0800

    Add distutils setup.py.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = "aur",
+    version = "0.8.0",
+    description = "Arch User Repository API library",
+    url = "https://github.com/cdown/pyaur/",
+
+    author = "Chris Down",
+    author_email = "chris@chrisdown.name",
+    license = "MIT",
+
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+    ],
+
+    packages = [
+        "aur"
+    ],
+)
