c1122ee4006d0ee2c67a7f506d2ac26a919c15e7
Chris Down <chris@chrisdown.name>
2013-08-24 12:14:10 +0200

    Vertically split docstrings in exceptions.py

--- aur/exceptions.py
+++ aur/exceptions.py
@@ -1,31 +1,44 @@
 #!/usr/bin/env python
 
+
 class QueryTooShortError(Exception):
-    """Raised when the query string is too short."""
+    """
+    Raised when the query string is too short.
+    """
     pass
 
 
 class UnknownAURError(Exception):
-    """Raised when we receive an unknown AUR error."""
+    """
+    Raised when we receive an unknown AUR error.
+    """
     pass
 
 
 class UnexpectedResponseTypeError(Exception):
-    """Raised when we receive an response type that is inappropriate for our
-       request."""
+    """
+    Raised when we receive an response type that is inappropriate for our
+    request.
+    """
     pass
 
 
 class UnknownPackageError(Exception):
-    """Raised when we make an info query, but the package does not exist."""
+    """
+    Raised when we make an info query, but the package does not exist.
+    """
     pass
 
 
 class InvalidCategoryIDError(Exception):
-    """Raised when an invalid category ID is given."""
+    """
+    Raised when an invalid category ID is given.
+    """
     pass
 
 
 class InvalidCategoryNameError(Exception):
-    """Raised when an invalid category name is given."""
+    """
+    Raised when an invalid category name is given.
+    """
     pass
