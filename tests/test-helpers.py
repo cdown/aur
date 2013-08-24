be5f31d39fd9768589b1f5dbf11561bc0650200f
Chris Down <chris@chrisdown.name>
2013-08-24 11:37:25 +0200

    PEP8 compliance in test-helpers.py.

--- tests/test-helpers.py
+++ tests/test-helpers.py
@@ -5,24 +5,24 @@ import aur
 
 a = aur.AURClient()
 
-yturlCategoryID = a.info("yturl").categoryID
+yturl_category_id = a.info("yturl").categoryID
 
-def testCategoryIDToCategoryName():
-    assert aur.helpers.category_id_to_name(yturlCategoryID) == "multimedia"
+def test_category_id_to_name():
+    assert aur.helpers.category_id_to_name(yturl_category_id) == "multimedia"
 
-def testCategoryNameToCategoryID():
+def test_category_name_to_id():
     assert aur.helpers.category_name_to_id(
-               aur.helpers.category_id_to_name(yturlCategoryID)
-           ) == yturlCategoryID
+               aur.helpers.category_id_to_name(yturl_category_id)
+           ) == yturl_category_id
 
 
-def testBadCategoryIDToCategoryName():
+def test_bad_category_id():
     assert_raises(
         aur.exceptions.InvalidCategoryIDError,
         aur.helpers.category_id_to_name, 9999
     )
 
-def testBadCategoryNameToCategoryID():
+def test_bad_category_name():
     assert_raises(
         aur.exceptions.InvalidCategoryNameError,
         aur.helpers.category_name_to_id, "nonexistent"
