05fbed5d4ef141ed5184a342cb97b484ccc8a1d6
Chris Down <chris@chrisdown.name>
2013-08-25 17:47:32 +0200

    Simplify package.py by setting kwargs attributes in a loop

--- aur/package.py
+++ aur/package.py
@@ -1,28 +1,22 @@
 #!/usr/bin/env python
 
-import datetime
+from datetime import datetime
 
 
 class Package(object):
     """
     Represents an AUR package and its respective metadata.
     """
-    def __init__(self, num_votes, description, url_path, last_modified, name, out_of_date,
-                 id, first_submitted, maintainer, version, category_id,
-                 license, url):
-        self.num_votes = num_votes
-        self.description = description
-        self.url_path = url_path
-        self.last_modified = datetime.datetime.utcfromtimestamp(last_modified)
-        self.name = name
+    def __init__(self, num_votes, description, url_path, last_modified, name,
+                 out_of_date, id, first_submitted, maintainer, version,
+                 category_id, license, url):
+        for k, v in locals().items():
+            if k != "self":
+                setattr(self, k, v)
+
+        self.first_submitted = datetime.utcfromtimestamp(first_submitted)
+        self.last_modified = datetime.utcfromtimestamp(last_modified)
         self.out_of_date = bool(out_of_date)
-        self.id = id
-        self.first_submitted = datetime.datetime.utcfromtimestamp(first_submitted)
-        self.maintainer = maintainer
-        self.version = version
-        self.category_id = category_id
-        self.license = license
-        self.url = url
 
     def __repr__(self):
         return "%s(%r)" % (self.__class__.__name__, self.__dict__)
