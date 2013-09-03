1e3a4926197ac71bec2b04e530d26ba6df2df82b
Chris Down <chris@chrisdown.name>
2013-09-03 19:13:06 +0200

    Release v0.9.0.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = "aur",
-    version = "0.8.0",
+    version = "0.9.0",
     description = "Arch User Repository API library",
     url = "https://github.com/cdown/pyaur/",
 
