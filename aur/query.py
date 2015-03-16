2708a469e4259e89a3db959b55652ff1da02b673
Chris Down <chris@chrisdown.name>
2015-03-16 00:35:38 +0000

    Use HTTPS when querying API

--- aur/query.py
+++ aur/query.py
@@ -103,7 +103,7 @@ def _query_api(query, query_type, multi=False):
         query_key += "[]"
 
     res_handle = requests.get(
-        "http://aur.archlinux.org/rpc.php?" + urlencode({
+        "https://aur.archlinux.org/rpc.php?" + urlencode({
             "type": query_type,
             query_key: query
         }, doseq=True)
