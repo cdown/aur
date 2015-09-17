92e1a5e83688fe2e7ac87cb883aac1c17f8e1a6b
Chris Down <chris@chrisdown.name>
2015-09-17 12:16:24 +0100

    Raise exception if we requested a package that isn't in the response

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import calendar
-import types
 import json
+import types
 import os
 import aur
 import httpretty
@@ -19,32 +19,40 @@ SAMPLE_DIR = os.path.join(os.path.dirname(__file__), 'samples')
     'msearch_found',
     'msearch_not_found',
     'multiinfo_found',
-    'multiinfo_not_found',
-    'multiinfo_only_partial_found',
+    ('multiinfo_not_found', aur.MissingPackageError),
+    ('multiinfo_only_partial_found', aur.MissingPackageError),
     'search_found',
     'search_not_found',
 ])
 @httpretty.activate
-def test_api_methods(test_file):
+def test_api_methods(test_file, should_raise_exc=None):
     method_name_to_test = test_file.split('_')[0]
     method_to_test = getattr(aur, method_name_to_test)
 
     api_resp_file = test_file + '.api_response'
     expected_file = test_file + '.expected'
+    args_file = test_file + '.args'
 
     with open(os.path.join(SAMPLE_DIR, api_resp_file)) as resp_f:
         api_resp = resp_f.read()
     with open(os.path.join(SAMPLE_DIR, expected_file)) as expected_f:
         expected = json.load(expected_f)
+    with open(os.path.join(SAMPLE_DIR, args_file)) as args_f:
+        args = json.load(args_f)
 
-    uri_regex = re.compile(r'/rpc\.php')
     httpretty.register_uri(
-        httpretty.GET, uri_regex,
+        httpretty.GET, re.compile(r'/rpc\.php'),
         body=api_resp, content_type='application/json',
     )
 
-    got = method_to_test('hunter2')
-    if isinstance(got, types.GeneratorType):
+    if should_raise_exc is not None:
+        with assert_raises(should_raise_exc):
+            method_to_test(args)
+        return
+    else:
+        got = method_to_test(args)
+
+    if isinstance(got, list) or isinstance(got, types.GeneratorType):
         # We need to compare the data, not the generator itself, so run it.
         got = [vars(package) for package in got]
     else:
